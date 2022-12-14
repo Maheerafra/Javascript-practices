7999e227db51658d4115f664c315d9c2b2f4216a
Maheerafra <abdiqafardiriyeafra@gmail.com>
2022-12-14 15:49:17 +0300

    added the console.log part

--- javascript.js
+++ javascript.js
@@ -3,19 +3,29 @@
 console.log("Hello, World!")
 
 let message = 'Hello!'; // define the variable and assign the value
-
+console.log('message')
 let admin, name; // can declare two variables at once
 
 name = "John";
 
 admin = name;
 
-alert( admin ); // "John"
+console.log(admin)
+
+
 
 let ourPlanetName = 'earth';
 
+console.log(ourPlanetName)
+
 let currentUserName = 'john';
+
+console.log(currentUserName)
+
 
 const BIRTHDAY = '18.04.1982';
 
-const age = someCode(birthday);
+console.log(BIRTHDAY)
+
+let a = 10
+console.log(a)
