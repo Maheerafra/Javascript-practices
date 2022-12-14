02d2c23aac47d22e6d61ec40eb17f5fe975821fd
Maheerafra <abdiqafardiriyeafra@gmail.com>
2022-12-14 14:11:14 +0300

    variables practice

---
+++ javascript.js
@@ -0,0 +1,21 @@
+
+// Your JavaScript goes here!
+console.log("Hello, World!")
+
+let message = 'Hello!'; // define the variable and assign the value
+
+let admin, name; // can declare two variables at once
+
+name = "John";
+
+admin = name;
+
+alert( admin ); // "John"
+
+let ourPlanetName = 'earth';
+
+let currentUserName = 'john';
+
+const BIRTHDAY = '18.04.1982';
+
+const age = someCode(birthday);
